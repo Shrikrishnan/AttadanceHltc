a41f402e75eaae573a90f79945112c131c9aae11
Shrikrishnan Balakrishnan <b.shrikrishnan@gmail.com>
2023-10-24 08:44:38 +0530

    Fist Commit

--- src/main/java/com/attadanceHltc/MainPage.java
+++ src/main/java/com/attadanceHltc/MainPage.java
@@ -2,10 +2,9 @@ package com.attadanceHltc;
 public class MainPage {
     public static void main(String[] args) {
         WebDriverManager.chromedriver().setup();
-
         ChromeDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("http://leaftaps.com/opentaps/");
+        driver.get("https://hltc.formulahr.co.in/Login.aspx");
     }
 }
-}
+
