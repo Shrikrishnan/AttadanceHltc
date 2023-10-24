d35aba04ea988461f8f2e613f97c4023f068870d
Shrikrishnan Balakrishnan <b.shrikrishnan@gmail.com>
2023-10-24 08:55:40 +0530

    Working Driver
    Signed-off-by: Shrikrishnan Balakrishnan <b.shrikrishnan@gmail.com>

--- src/main/java/com/attadanceHltc/MainPage.java
+++ src/main/java/com/attadanceHltc/MainPage.java
@@ -1,4 +1,8 @@
 package com.attadanceHltc;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class MainPage {
     public static void main(String[] args) {
         WebDriverManager.chromedriver().setup();
