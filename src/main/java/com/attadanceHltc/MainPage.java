37bb1d34621372cd044206ac59f481256c389018
Shrikrishnan Balakrishnan <17940323+Shrikrishnan@users.noreply.github.com>
2024-01-01 23:47:48 +0530

    Update MainPage.java
    removal of unwanted lines

--- src/main/java/com/attadanceHltc/MainPage.java
+++ src/main/java/com/attadanceHltc/MainPage.java
@@ -7,8 +7,7 @@ public class MainPage {
     public static void main(String[] args) {
         WebDriverManager.chromedriver().setup();
         ChromeDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://hltc.formulahr.co.in/Login.aspx");
+        driver.manage().window().maximize();  driver.get("https://hltc.formulahr.co.in/Login.aspx");
     }
 }
 
